936022f14ea078d0e9c47c50e94c9cd41aecf48b
Joshua Lane <jlane@fastly.com>
2016-06-08 22:39:15 +0000

    DRY up callback spec

--- spec/policy_callback_spec.rb
+++ spec/policy_callback_spec.rb
@@ -13,19 +13,23 @@ RSpec.describe 'Critic::Policy', 'callbacks' do
   end
   let!(:resource) { Struct.new(:id)  }
 
+  def authorize(id)
+    policy.authorize(:show, nil, resource.new(id)).result
+  end
+
   it 'raises AuthorizationDenied if before_authorize hook returns false' do
     policy.before_authorize { |policy| nil }
     policy.before_authorize { |policy| "woo" }
     policy.before_authorize { |policy| policy.resource.id != 5 }
 
-    expect(policy.authorize(:show, nil, resource.new(1)).result).to eq(true)
-    expect(policy.authorize(:show, nil, resource.new(5)).result).to eq(false)
+    expect(authorize(1)).to eq(true)
+    expect(authorize(5)).to eq(false)
   end
 
   it 'does not raise AuthorizationDenied on after_authorize hooks' do
     policy.after_authorize { |policy| policy.resource.id.nil? }
 
-    expect(policy.authorize(:show, nil, resource.new(5)).result).to eq(true)
-    expect(policy.authorize(:show, nil, resource.new(nil)).result).to eq(true)
+    expect(authorize(5)).to eq(true)
+    expect(authorize(nil)).to eq(true)
   end
 end
